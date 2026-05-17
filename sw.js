7a81a7d9491e09ce830e4eb54c59da4a85d0ec55
higiscuidandodevoce-dot <higiscuidandodevoce@gmail.com>
2026-05-17 17:36:31 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'lowcarb-v1';
+const CACHE = 'marcia-lowcarb-v1';
 const FILES = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/marciaguialowcarb/',
+  '/marciaguialowcarb/index.html',
+  '/marciaguialowcarb/manifest.json',
+  '/marciaguialowcarb/icon-192.png',
+  '/marciaguialowcarb/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
@@ -23,6 +23,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/marciaguialowcarb/index.html')))
   );
 });
